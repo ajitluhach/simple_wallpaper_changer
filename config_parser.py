72efe6ea0ee86618ebea9b2a07c2f5d0aeb05449
Ajit Singh <ajit96singh@gmail.com>
2017-04-04 21:16:25 +0530

    Edited the config file and changed the wallpaper

--- config_parser.py
+++ config_parser.py
@@ -1,4 +1,37 @@
 from pexel import *
 import subprocess
-import PyQt5
+import shutil
+
+
+#Config.read('/home/wolfmeist/.config/plasma-org.kde.plasma.desktop-appletsrc')
+#get the image file name
 image_location = resize_image()
+filename = '/home/wolfmeist/.config/plasma-org.kde.plasma.desktop-appletsrc'
+#temporary file to store the edits
+outputfile = 'plasma-org.kde.plasma.desktop-appletsrc'
+Edited = False
+with open(filename) as fin, open(outputfile, 'w+') as out:
+    flag = False
+    for line in fin:
+        out.write(line)
+        # if group wallpaper found
+        if 'Wallpaper' in line:
+            # second line is the image file name
+            new_line = 'Image=file://'+image_location+'\n'
+            out.write(new_line)
+            Edited = True
+            # skip the next line to read
+            line = next(fin)
+            continue
+
+outputfile = os.getcwd() + "/"+outputfile
+#move the temporary file with the orignal appletrc file
+if Edited:
+    shutil.move(outputfile, filename)
+
+#restart the plasma shell
+kill = "killall plasmashell"
+start = "kstart plasmashell"
+
+subprocess.call(kill, shell=True)
+subprocess.call(start, shell=True)
