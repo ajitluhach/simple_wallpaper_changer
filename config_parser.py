170ece599208330ed3b24d6f9631c30d14e4ab26
Ajit Singh <ajit96singh@gmail.com>
2017-04-04 21:20:45 +0530

    Some code refactoring

--- config_parser.py
+++ config_parser.py
@@ -2,12 +2,11 @@ from pexel import *
 import subprocess
 import shutil
 
-
-#Config.read('/home/wolfmeist/.config/plasma-org.kde.plasma.desktop-appletsrc')
-#get the image file name
+# Config.read('/home/wolfmeist/.config/plasma-org.kde.plasma.desktop-appletsrc')
+# get the image file name
 image_location = resize_image()
 filename = '/home/wolfmeist/.config/plasma-org.kde.plasma.desktop-appletsrc'
-#temporary file to store the edits
+# temporary file to store the edits
 outputfile = 'plasma-org.kde.plasma.desktop-appletsrc'
 Edited = False
 with open(filename) as fin, open(outputfile, 'w+') as out:
@@ -17,19 +16,19 @@ with open(filename) as fin, open(outputfile, 'w+') as out:
         # if group wallpaper found
         if 'Wallpaper' in line:
             # second line is the image file name
-            new_line = 'Image=file://'+image_location+'\n'
+            new_line = 'Image=file://' + image_location + '\n'
             out.write(new_line)
             Edited = True
             # skip the next line to read
             line = next(fin)
             continue
 
-outputfile = os.getcwd() + "/"+outputfile
-#move the temporary file with the orignal appletrc file
+outputfile = os.getcwd() + "/" + outputfile
+# move the temporary file with the orignal appletrc file
 if Edited:
     shutil.move(outputfile, filename)
 
-#restart the plasma shell
+# restart the plasma shell
 kill = "killall plasmashell"
 start = "kstart plasmashell"
 
--- pexel.py
+++ pexel.py
@@ -1,10 +1,11 @@
-from bs4 import BeautifulSoup
+import getpass
 from urllib.request import urlopen, Request
+
+import os
 import random
 import re
-import os
-import getpass
 from PIL import Image
+from bs4 import BeautifulSoup
 
 base_url = "http://www.pexels.com"
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
@@ -34,7 +35,7 @@ def get_search_url():
 
 def get_image_links(url):
     """get all the image links on the page"""
-    #Soup the soup first
+    # Soup the soup first
     soup = make_soup(url)
     regex = r"href=\"(/.*/)\""
     reg = re.compile(regex)
@@ -48,13 +49,13 @@ def get_image_links(url):
 def get_one():
     """Return one Image Link from all the image links"""
     global i_name
-    #get the search URL
+    # get the search URL
     url = get_search_url()
-    #get all the image addresses on that URL
+    # get all the image addresses on that URL
     addresses = get_image_links(url)
-    #Choose one image from the addresses
+    # Choose one image from the addresses
     image = str(random.choice(addresses))
-    #Print the image Name to be downloaded
+    # Print the image Name to be downloaded
     i_name = image.split("/")[-2]
     i_name += ".jpeg"
     print("Image Name : ", i_name)
@@ -63,7 +64,7 @@ def get_one():
 
 def this_image():
     """get the download link of the image"""
-    #Make soup of the chosen image URL
+    # Make soup of the chosen image URL
     soup = make_soup(get_one())
     address = str(soup.find("form", id="download-size"))
     regex = r"""data-alt-url=\"(.*)\" data-name=\"original\""""
@@ -76,9 +77,9 @@ def download_image():
     """Download the Image"""
     print("Started Fetching")
     download_url = this_image()
-    #get users name to find the config file and saving the downloaded files in Desktop/.desktop
+    # get users name to find the config file and saving the downloaded files in Desktop/.desktop
     username = getpass.getuser()
-    #Save location of Original downloaded file
+    # Save location of Original downloaded file
     location = "/home/" + username + "/Desktop/.desktop/originals/"
     if not os.path.isdir(location):
         os.mkdir(location)
@@ -96,11 +97,9 @@ def resize_image():
     loc, name = download_image()
     I = Image.open(loc)
     I = I.resize((1366, 768), Image.ANTIALIAS)
-    name  = loc.split('/')[-1]
-    location = "/".join(loc.split('/')[:-2])+"/"
+    name = loc.split('/')[-1]
+    location = "/".join(loc.split('/')[:-2]) + "/"
     name = name.split('.')[0]
     name = location + name + "-1366x768.jpeg"
     I.save(name, quality=100)
     return name
-
-
